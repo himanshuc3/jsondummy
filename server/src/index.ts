f24ee5638055876a6c34876ce6fa8c6f6e6c219b
Himanshu Chhabra <himichhabra14@gmail.com>
2024-10-01 15:11:28 +0530

    Changes from personal

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,43 @@
 
 import { config as dotenvConfig } from 'dotenv'
+import { generateRandomData } from 'datagen-rs-node';
+
 dotenvConfig()
 
 import config from './config'
 
 import app from './server'
+
+// async function generateData() {
+//     const randomData = await generateRandomData({
+//         "options": {
+//             "serializer": {
+//                 "type": "json",
+//                 "pretty": true
+//             }
+//         },
+//         "type": "object",
+//         "properties": {
+//             "test": {
+//                 "type": "number",
+//                 "min": 30,
+//                 "max": 55
+//             },
+//             "productName": {
+//                 "type": "string",
+//                 "generator": {
+//                     "type": "firstName"
+//                 }
+//             }
+//         }
+//     }, ({ current, total }) => {
+//         console.log(`Generated ${current}/${total} items`);
+//     })
+//     console.log(randomData)
+// }
+
+// generateData()
+
 
 
 
